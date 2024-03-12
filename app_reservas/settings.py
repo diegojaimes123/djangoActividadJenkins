3f03e51a676e8c167bd5f3f19906c69b53ef737c
diegojaimes123 <110691669+diegojaimes123@users.noreply.github.com>
2024-03-12 15:55:31 -0500

    Update settings.py

--- app_reservas/settings.py
+++ app_reservas/settings.py
@@ -143,10 +143,8 @@ REST_FRAMEWORK = {
 # Asegúrate de incluir aquí los dominios que corresponden al Front-End y Back-End de tu aplicación.
 # Esto es esencial para permitir solicitudes desde diferentes dominios, como el Front-End y Back-End,
 # y evitar problemas de política de mismo origen (Same-Origin Policy).
-CORS_ALLOWED_ORIGINS = (
-    "http://localhost:65500",  # Dominio del componente Front-End
-    "http://localhost:8000",  # Dominio del componente Back-End
-)
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Dominio del componente Front-End
 CSRF_TRUSTED_ORIGINS = ["http://localhost:65500"]
